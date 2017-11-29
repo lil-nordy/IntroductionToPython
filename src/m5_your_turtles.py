835cf4c0a51525447bfd0c3ec0c6f2a285a71396
nordqunn <nordqunn@rose-hulman.edu>
2017-11-28 23:30:18 -0500

    Completed all modules to specification.

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -12,7 +12,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 ## when I have the speed set to accummulate by an interval of 10 on each iteration of the loop, but doesn't increase at
 ## all when I set the  speed to accumulate by an interval of 9 on each iteration? strange.
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -34,23 +34,23 @@ import rosegraphics as rg
 
 window = rg.TurtleWindow()
 
+erik2 = rg.SimpleTurtle('turtle')
+erik2.paint_bucket = rg.PaintBucket('red')
+
+speed = 10
+angle = 0
 radius = 50
-speed = 1
-step = 0
-erik = rg.SimpleTurtle('turtle')
-erik.paint_bucket = rg.PaintBucket('red')
 
-
-for k in range(10):
-    erik.speed = speed + 10
-# speed, integer from 1 - 10 in the doc-string  of the rose-graphics.py file, but seems to respond to much higher numbers
-    erik.pen_down()
-    erik.begin_fill()
-    erik.draw_circle(radius)
+for k in range(25):
+    erik2.pen_down()
+    erik2.begin_fill()
+    erik2.speed = speed + 10
+    erik2.draw_circle(radius)
     radius = radius + 5
-    step = step + 5
-    erik.forward(step)
-    erik.end_fill()
+    angle = angle + 5
+    erik2.left(angle)
+    erik2.end_fill()
+
 
 nate = rg.SimpleTurtle('turtle')
 nate.paint_bucket = rg.PaintBucket('green')
@@ -68,12 +68,10 @@ for k in range(25):
     nate.begin_fill()
     nate.speed = speed + 10
     nate.draw_circle(radius)
-    radius = radius + 5
+    radius = radius + 3
     angle = angle + 5
     nate.left(angle)
     nate.end_fill()
-
-
 
 window.close_on_mouse_click()
 
